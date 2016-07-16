3e2657ef1fcdfa9199ad96be9291f601afab9cef
luque-gh <luque.gh@gmail.com>
2016-07-16 15:27:30 -0300

    Inicializando
    Projeto contendo 1 jogador e 3 bots
    Ainda precisa ser refatorado

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject hoverCameraHelper;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LateUpdate()
+    {
+        //Costy!!!
+        GameObject[] hoverList = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject hover in hoverList) 
+        {
+            if (hover.GetComponent< HoverController >().IsAlive())
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                foreach (Transform child in hover.transform)
+                {
+                    GameObject childGO = child.gameObject;
+                    if (child.gameObject.name == hoverCameraHelper.name)
+                    {
+                        transform.position = childGO.transform.position;
+                        transform.rotation = childGO.transform.rotation;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotController : MonoBehaviour
+{
+
+    public float velocity = 75.0f;
+    public float maxLifetime = 3.0f;
+    public float damage = 10.0f;
+    private float lifeTime = 0;
+
+    public void setInitialVelocity()
+    {
+        Rigidbody shotRb = GetComponent<Rigidbody>();
+        shotRb.velocity = transform.TransformDirection(-Vector3.up) * velocity;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //Não funcionar setando aqui a velocidade inicial, pois perde um update de física
+    }
+
+    void FixedUpdate()
+    {
+        lifeTime += Time.fixedDeltaTime;
+        //Usando up por causa de capsula tosca!
+        //transform.Translate(-Vector3.up * velocity * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeTime > maxLifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "IA" ||
+            other.gameObject.tag == "Player")
+        {
+            HoverController hc = other.gameObject.GetComponent<HoverController>();
+            if (hc != null)
+            {
+                hc.Damage(damage, transform.position);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
